e4ded07a762642fb4651ed286d0c9e414e5af9ff
Tsungyu Yu <ytyubox@gmail.com>
2020-09-04 23:29:56 +0800

    int, nat

--- Sources/FunctionalParser/FunctionalParser.swift
+++ Sources/FunctionalParser/FunctionalParser.swift
@@ -1,3 +1,61 @@
+import Foundation
 struct FunctionalParser {
     var text = "Hello, World!"
+}
+
+protocol ParserProtocol {
+    func parse(_ str:String) -> Bool
+}
+struct Q<T>:Equatable where T: Equatable {
+    internal init(_ value: T, _ next: String) {
+        self.value = value
+        self.next = next
+    }
+    
+    var value:T
+    var next:String
+}
+//typealias Q<T> = (T, String)
+typealias P<T> = [Q<T>] where T:Equatable
+func someDigit(_ str:String) -> P<UInt> {
+    
+    [parse(str, IsNumber())].compactMap{$0}
+        
+//    return []// UInt(i >= 0 ? i : 0)
+}
+
+//func someInt(_ str:String) ->Int {
+//    var str = str
+//    let c = str.removeFirst()
+//
+//}
+protocol Logic {
+    associatedtype Output
+    func logic(_:Character) -> Bool
+    func transform(_:String) -> Output
+}
+func parse<L>(_ str:String,_ logic:L) -> Q<L.Output>?
+where L:Logic
+{
+    var o = [Character]()
+    var copy = str
+    for c in str {
+        guard logic.logic(c) else  {
+            break
+        }
+        o.append(copy.removeFirst())
+    }
+    if o.isEmpty {return nil}
+    let v = String(o)
+    return Q(logic.transform(v), copy)
+}
+
+struct IsNumber: Logic {
+    typealias Output = UInt
+    func logic(_ c: Character) -> Bool {
+        return "1234567890".contains(c)
+    }
+    func transform(_ s: String) -> Output {
+        Output(s) ?? 0
+    }
 }
--- Tests/FunctionalParserTests/FunctionalParserTests.swift
+++ Tests/FunctionalParserTests/FunctionalParserTests.swift
@@ -2,14 +2,41 @@ import XCTest
 @testable import FunctionalParser
 
 final class FunctionalParserTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FunctionalParser().text, "Hello, World!")
+    //    func testSomeInt() {
+    //        XCTAssertEqual(someInt("123"), 123)
+    //        XCTAssertEqual(someInt("1abc"), 1)
+    //        XCTAssertEqual(someInt("-123"), -123)
+    //
+    //    }
+    func testIsNumber() {
+        let isNumber = IsNumber()
+        for i:UInt in 0...9 {
+            let c = Character("\(i)")
+            let logicResult = isNumber.logic(c)
+            XCTAssertTrue(logicResult)
+            let transformOutput = isNumber.transform("\(c)")
+            XCTAssertEqual(transformOutput, i)
+        }
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func test_parse_success() throws {
+        let logic = IsNumber()
+        let parseResult = try XCTUnwrap(parse("123", logic))
+        
+        
+        XCTAssertEqual(parseResult.value, 123)
+        XCTAssertEqual(parseResult.next, "")
+    }
+    func test_parse_Will_failure() throws {
+        let logic = IsNumber()
+        let parseResult = try XCTUnwrap(parse("1abc", logic))
+        XCTAssertEqual(parseResult.value, 1)
+        XCTAssertEqual(parseResult.next, "abc")
+    }
+    func testSomeDigit() {
+        XCTAssertEqual(someDigit("123"), [Q(123,"")])
+        XCTAssertEqual(someDigit("1abc"), [Q(1,"abc")])
+        XCTAssertEqual(someDigit("-123"), [])
+    }
+    
 }
+
